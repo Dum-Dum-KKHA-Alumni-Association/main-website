f8b7359ad055378c8be26c4c329ebbb2d6619bc3
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2025-01-29 20:59:41 +0530

    fix: homepage format details

--- src/app/(home)/Videos.tsx
+++ src/app/(home)/Videos.tsx
@@ -21,7 +21,7 @@ const VideosSection = async () => {
 		<section className="w-full bg-foreground py-24">
 			<section className="m-auto flex w-full max-w-7xl flex-col items-center justify-between gap-10 px-5">
 				<section>
-					<h2 className="text-white">Gallery</h2>
+					<h2 className="text-white">Our Videos</h2>
 				</section>
 				<section className="grid w-full grid-cols-1 gap-6 md:grid-cols-3">
 					{sortByISOString(videoData)
